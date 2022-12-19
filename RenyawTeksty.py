c123c19511ab3bcc427f2565ebcc48dd6231436c
RenyaWoz <renyaw@interia.pl>
2022-12-19 13:05:22 +0100

    1912-12: 21Typy w Pythonie

--- RenyawTeksty.py
+++ RenyawTeksty.py
@@ -1,1 +1,23 @@
+''''
+Komentarz
+komentarz
+'''
+
 imie = "Renia"
+wiek = "18"
+print(type(imie))
+print(type(wiek))
+print("Wynik mnożenia:", wiek * 2)  # tu jest błąd!
+print("Wynik łączenia:", imie + wiek)
+print(imie.upper())
+print(imie.lower())
+print(imie.title())
+print(imie.count("e"))
+print(imie.join("---"))
+print(imie.rstrip())
+
+
+
+
+
+
