70edd35115ca11f4f3c82199192c143bad339816
RenyaWoz <renyaw@interia.pl>
2022-12-20 12:26:06 +0100

    1912-12: 21Typy w Pythonie

--- FunkcjeRW.py
+++ FunkcjeRW.py
@@ -4,6 +4,11 @@ def drukuj_napis():
 
 drukuj_napis()
 
-def drukarka (tekst, ile_razy):
-    return
 
+def drukarka(tekst, ile_razy):
+    wydruk = (tekst + " ") * ile_razy
+    return wydruk
+
+
+lista = [drukarka("RENIA", 10), drukarka("RENIA", 5), drukarka("RENIA", 2), drukarka("RENIA", 10), 'YETI']
+print(lista)
