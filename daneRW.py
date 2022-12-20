1e6b094ea57f2ca5f5e47e5bdc7d2f46831fe045
RenyaWoz <renyaw@interia.pl>
2022-12-20 15:13:14 +0100

    2012-12: 21Typy w Pythonie

--- daneRW.py
+++ daneRW.py
@@ -2,31 +2,36 @@
 # #plik =open('klienci.txt','w')
 # plik = open('klienci.txt' , 'a')
 
-with open('klienci.txt,'r') as plik:
+with open('klienci.txt','a') as plik:
+    plik.write("Renia")
+with open('klienci.txt','r') as plik:
     nowa_lista=[]
-    imie = plik.redline().rstrip
-#
+    imie = plik.readline().rstrip('\n')
+    while imie!="":
+        nowa_lista.append(imie)
+        imie = plik.readline().rstrip("\n")
+
 # lista =['Genowefa','Ziuta']
 # name = 'Stefania Janik'
 # wiek = 41
 #
 # plik.write(name + "\n")
-# plik.write(str(wiek)+ "\n")
-# plik.write(str(lista)+ "\n")
-
-nowa_lista = []
-zawartosc1 = plik.readline().rstrip('\n')
-zawartosc2 = plik.readline().rstri('\n')
-zawartosc3 = plik.readline().rstrip('\n')
-zawartosc4 = plik.readline().rstrip('\n')
-
-nowa_lista.append(plik.readline() .rstrip('\n'))
-nowa_lista.append(plik.readline() .rstrip('\n'))
-nowa_lista.append(plik.readline() .rstrip('\n'))
-nowa_lista.append(plik.readline() .rstrip('\n'))
-
-
-
-
-plik.close()
+# # plik.write(str(wiek)+ "\n")
+# # plik.write(str(lista)+ "\n")
+#
+# nowa_lista = []
+# zawartosc1 = plik.readline().rstrip('\n')
+# zawartosc2 = plik.readline().rstri('\n')
+# zawartosc3 = plik.readline().rstrip('\n')
+# zawartosc4 = plik.readline().rstrip('\n')
+#
+# nowa_lista.append(plik.readline() .rstrip('\n'))
+# nowa_lista.append(plik.readline() .rstrip('\n'))
+# nowa_lista.append(plik.readline() .rstrip('\n'))
+# nowa_lista.append(plik.readline() .rstrip('\n'))
+#
+#
+#
+#
+# plik.close()
 print(nowa_lista)
