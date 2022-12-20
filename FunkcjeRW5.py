26a9c29c7ab4f4257e4230464adc1718c83ecec3
RenyaWoz <renyaw@interia.pl>
2022-12-20 14:20:40 +0100

    2012-12: 21Typy w Pythonie

--- FunkcjeRW5.py
+++ FunkcjeRW5.py
@@ -8,9 +8,11 @@ lista(  11111,11111  )
 lista(  1111111,11111111  )
 
 
-def lista(imie,*args):
+def lista(imie="" ,/,*args, **kwargs):
     print(imie)
     print(args)
+    print(kwargs)
 lista('YETI')
-lista(1)
-lista(1,11)
+lista('YETI',1)
+lista("Teofil",1, imie ='YETI')
+lista('YETI',1,11)
