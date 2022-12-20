4f293499306857dc5c6097b346bd55670100ad22
RenyaWoz <renyaw@interia.pl>
2022-12-20 11:46:10 +0100

    1912-12: 21Typy w Pythonie

---
+++ dopokiRW.py
@@ -0,0 +1,22 @@
+# while True:
+#     print('''
+#     :-)----Renia---:-)
+#     1.drukuj napis x2
+#     2.drukuj napis x3
+#     3,zakończ
+#     ''')
+#     wybór = input("Podaj swój wybór 1-3:")
+#     if wybór == "1":
+#         print("NAPIS "*2)
+#     elif wybór =="2":
+#         print("NAPIS "*3)
+#     elif wybór =="3":
+#         break
+#     else:
+#         print("Zły wybór , spróbuj ponownie!")
+
+licznik = 0
+
+while licznik < 5:
+    print(f"licznik mniejszy od {licznik}")
+    licznik += 1
