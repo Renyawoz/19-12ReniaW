f60d9cb5a6fcf534fc0dbec6e921c2190da1a9f9
RenyaWoz <renyaw@interia.pl>
2022-12-20 14:49:20 +0100

    2012-12: 21Typy w Pythonie

---
+++ daneRW.py
@@ -0,0 +1,22 @@
+# #plik = open('klienci.txt','r')
+# #plik =open('klienci.txt','w')
+# plik = open('klienci.txt' , 'a')
+#
+# lista =['Genowefa','Ziuta']
+# name = 'Stefania Janik'
+# wiek = 41
+#
+# plik.write(name + "\n")
+# plik.write(str(wiek)+ "\n")
+# plik.write(str(lista)+ "\n")
+
+nowa_lista = []
+zawartosc1 = plik.readline().rstrip ('\n'')
+zawartosc2 = plik.readline().rstrip ('\n'')
+zawartosc3 = plik.readline().rstrip ('\n'')
+zawartosc4 = plik.readline().rstrip ('\n'')
+
+
+
+
+plik.close()
