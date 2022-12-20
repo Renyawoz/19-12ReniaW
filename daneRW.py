969d2672f06f151aa57bc592e09a1356d7be166d
RenyaWoz <renyaw@interia.pl>
2022-12-20 15:03:46 +0100

    2012-12: 21Typy w Pythonie

--- daneRW.py
+++ daneRW.py
@@ -1,6 +1,10 @@
 # #plik = open('klienci.txt','r')
 # #plik =open('klienci.txt','w')
 # plik = open('klienci.txt' , 'a')
+
+with open('klienci.txt,'r') as plik:
+    nowa_lista=[]
+    imie = plik.redline().rstrip
 #
 # lista =['Genowefa','Ziuta']
 # name = 'Stefania Janik'
@@ -11,12 +15,18 @@
 # plik.write(str(lista)+ "\n")
 
 nowa_lista = []
-zawartosc1 = plik.readline().rstrip ('\n'')
-zawartosc2 = plik.readline().rstrip ('\n'')
-zawartosc3 = plik.readline().rstrip ('\n'')
-zawartosc4 = plik.readline().rstrip ('\n'')
+zawartosc1 = plik.readline().rstrip('\n')
+zawartosc2 = plik.readline().rstri('\n')
+zawartosc3 = plik.readline().rstrip('\n')
+zawartosc4 = plik.readline().rstrip('\n')
+
+nowa_lista.append(plik.readline() .rstrip('\n'))
+nowa_lista.append(plik.readline() .rstrip('\n'))
+nowa_lista.append(plik.readline() .rstrip('\n'))
+nowa_lista.append(plik.readline() .rstrip('\n'))
 
 
 
 
 plik.close()
+print(nowa_lista)
