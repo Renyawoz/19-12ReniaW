04492549f5c798af48931cd80b24af5c7f6988a1
RenyaWoz <renyaw@interia.pl>
2022-12-20 10:10:56 +0100

    1912-12: 21Typy w Pythonie

---
+++ petleRW.py
@@ -0,0 +1,39 @@
+#znaPythona = 0
+
+#if znaPythona:
+    #print("Gratulacje!")
+
+
+#else:
+#    print("Musisz się nauczyć")
+
+#print("...Kolejne instrukcje programu...")
+
+# znaPythona = input("Czy znasz jezyk? (Wprowadź 't' lub 'n')")
+#
+# if znaPythona == 'tak' or znaPythona == 't' or znaPythona == 'T':
+#     print("Gratulacje!")
+#
+#
+# elif znaPythona == 'nie' or znaPythona == 'n' or znaPythona == 'N' :
+#     print("Musisz się nauczyć")
+#
+# else :
+#     print("Nie ma takiego wyboru")
+#
+# print("...Kolejne instrukcje programu...")
+#
+# podatek = 1500
+# if podatek < 1000:
+#     opłata = 0.0
+#
+# elif podatek < 3000:
+#     opłata = 0.20
+#
+# elif podatek < 5000:
+#     opłata = 0.35
+#
+# else:
+#     opłata = 0.45
+# print(opłata)
+
