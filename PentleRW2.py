ce6749afcf65ed18730a950d03aae7ab3447fc82
RenyaWoz <renyaw@interia.pl>
2022-12-20 10:49:46 +0100

    1912-12: 21Typy w Pythonie

--- PentleRW2.py
+++ PentleRW2.py
@@ -1,6 +1,12 @@
 lista = [4, 6, 8, 0]
-
+wyniki = []
 for i in lista:
-    print(i*20)
+    wyniki.append(i*20)
+
+print(wyniki)
+
+for i in range (5):
     print(i)
 
+for i in range(100,200,50):
+    print(i)
