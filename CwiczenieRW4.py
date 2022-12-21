4e0c865f375aec2208600716420ff5a7f1683ceb
RenyaWoz <renyaw@interia.pl>
2022-12-21 11:39:37 +0100

    2012-16: 21Typy w Pythonie

--- CwiczenieRW4.py
+++ CwiczenieRW4.py
@@ -12,13 +12,18 @@ import csv
 #             licznik += 1
 #             print(f'Znalazłem{licznik}linii')
 
-with open(r'C:\Users\CSComarch\Documents\19-12ReniaW\1912_poniedziałekRW\dane\osoby.csv') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    licznik = 0
-    for row in csv_reader:
-        if licznik == 0:
-            print(f'Nazwy kolumn to{", ".join(row)}')
-            licznik +=1
-        print(f"\t{row['name']}|\t{row['department']}|\t{row['birthday month']}")
-        licznik += 1
-    print(f'Znalazłem{licznik}linii')
+# with open(r'C:\Users\CSComarch\Documents\19-12ReniaW\1912_poniedziałekRW\dane\osoby.csv') as csv_file:
+#     csv_reader = csv.DictReader(csv_file)
+#     licznik = 0
+#     for row in csv_reader:
+#         if licznik == 0:
+#             print(f'Nazwy kolumn to{", ".join(row)}')
+#             licznik +=1
+#         print(f"\t{row['name']}|\t{row['department']}|\t{row['birthday month']}")
+#         licznik += 1
+#     print(f'Znalazłem{licznik}linii')
+
+with open(r'C:\Users\CSComarch\Documents\19-12ReniaW\1912_poniedziałekRW\dane\osoby.csv','a') as file:
+    writer = csv.writer(file, delimiter=",", quoting = csv.QUOTE_MINIMAL)
+    writer.writerow(['Renia', 'IT', 'Czerwiec'])
+
