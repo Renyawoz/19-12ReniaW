2bbd04c588b21264ed2a3c53720a05554fd9038d
RenyaWoz <renyaw@interia.pl>
2022-12-21 11:31:31 +0100

    2012-16: 21Typy w Pythonie

--- CwiczenieRW4.py
+++ CwiczenieRW4.py
@@ -1,1 +1,24 @@
-import  cvs
+import csv
+
+# with open(r'C:\Users\CSComarch\Documents\19-12ReniaW\1912_poniedziałekRW\dane\osoby.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=",")
+#     licznik = 0
+#     for row in csv_reader:
+#         if licznik == 0:
+#             print(f'Nazwy kolumn to{", ".join(row)}')
+#             licznik +=1
+#         else:
+#             print(f'\t{row[0]}|\t{row[1]}|\t{row[2]}')
+#             licznik += 1
+#             print(f'Znalazłem{licznik}linii')
+
+with open(r'C:\Users\CSComarch\Documents\19-12ReniaW\1912_poniedziałekRW\dane\osoby.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    licznik = 0
+    for row in csv_reader:
+        if licznik == 0:
+            print(f'Nazwy kolumn to{", ".join(row)}')
+            licznik +=1
+        print(f"\t{row['name']}|\t{row['department']}|\t{row['birthday month']}")
+        licznik += 1
+    print(f'Znalazłem{licznik}linii')
